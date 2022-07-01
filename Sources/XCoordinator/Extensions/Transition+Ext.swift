06b8435d3d5f9357fbae2f798791c80227b0238f
Can Balkaya <canbalkya@gmail.com>
2022-07-01 13:23:55 +0300

    Create transition extension

---
+++ Sources/XCoordinator/Extensions/Transition+Ext.swift
@@ -0,0 +1,33 @@
+//
+//  Transition+Ext.swift
+//  RxSwift
+//
+//  Created by Can Balkaya on 7/1/22.
+//
+
+import UIKit
+import XCoordinator
+
+public extension Transition {
+
+    static func presentFullScreen(_ presentable: Presentable, animation: Animation? = nil) -> Transition {
+        presentable.viewController?.modalPresentationStyle = .fullScreen
+        return .present(presentable, animation: animation)
+    }
+
+    static func dismissAll() -> Transition {
+        return Transition(presentables: [], animationInUse: nil) { rootViewController, options, completion in
+            guard let presentedViewController = rootViewController.presentedViewController else {
+                completion?()
+                return
+            }
+            presentedViewController.dismiss(animated: options.animated) {
+                Transition
+                    .dismissAll()
+                    .perform(on: rootViewController,
+                             with: options,
+                             completion: completion)
+            }
+        }
+    }
+}
