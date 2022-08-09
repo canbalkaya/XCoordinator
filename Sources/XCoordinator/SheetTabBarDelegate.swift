0f6718e30ff6dd715f86336faf98495d487b1b9f
Can Balkaya <canbalkya@gmail.com>
2022-08-09 18:25:00 +0300

    Create SheetTabBarDelegate.swift

---
+++ Sources/XCoordinator/SheetTabBarDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SheetTabBarDelegate.swift
+//  
+//
+//  Created by Can Balkaya on 8/9/22.
+//
+
+import UIKit
+
+open class SheetTabBarDelegate: NSObject, UITabBarControllerDelegate {
+
+    // MARK: - Properties
+    public var didSelectSheet: (UIViewController) -> Void
+
+    // MARK: - Initialization
+    public init(didSelectSheet: @escaping (UIViewController) -> Void) {
+        self.didSelectSheet = didSelectSheet
+    }
+
+    // MARK: - Methods
+    open func tabBarController(
+        _ tabBarController: UITabBarController,
+        shouldSelect viewController: UIViewController
+    ) -> Bool {
+        if viewController.children.first is EmptyViewController {
+            didSelectSheet(viewController)
+            return false
+        } else {
+            return true
+        }
+    }
+}
