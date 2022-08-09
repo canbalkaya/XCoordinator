1f0d7d12247503e8f885ab199e0eba938deb65f8
Can Balkaya <canbalkya@gmail.com>
2022-08-09 18:24:54 +0300

    Create EmptyCoordinator.swift

---
+++ Sources/XCoordinator/EmptyCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Can Balkaya on 8/9/22.
+//
+
+import UIKit
+import XCoordinator
+
+open enum EmptyRoute: Route {
+    case empty
+}
+
+open class EmptyViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+open class EmptyCoordinator: NavigationCoordinator<EmptyRoute> {
+    
+    // MARK: - Initialization
+    public init() {
+        super.init(initialRoute: .empty)
+    }
+    
+    // MARK: - Overrides
+    open override func prepareTransition(for route: EmptyRoute) -> NavigationTransition {
+        let viewController = EmptyViewController()
+        return .push(viewController)
+    }
+}
